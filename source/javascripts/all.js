ec5a034cd27193a194834774da5e9804d881ec47
James Seymour-Lock <james@simpleasmilk.co.uk>
2014-12-12 15:59:38 +0100

    Basic time/date pickers

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,6 +1,5 @@
 //= require jquery.fs.stepper.js
 //= require select2.js
-//= require bootstrap-datetimepicker.min.js
 
 $(".form-control--stepper").stepper();
 $(".form-control--select").select2();
